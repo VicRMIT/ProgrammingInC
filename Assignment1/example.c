1b7d58591f3420b51eafbb65528d859aa0c6a021
VicRMIT <s3732094@student.rmit.edu.au>
2018-12-10 15:17:09 +1100

    Add starting files to repo

---
+++ Assignment1/example.c
@@ -0,0 +1,13 @@
+//This is a comment
+#include <stdio.h>
+
+int main()
+{
+    if ( 1 > 0 ) {
+    //for(count=0; count < DASHES; count++) putchar('-'); 
+        printf("Hello, World!");
+        return 0;
+    }
+}
+
+
