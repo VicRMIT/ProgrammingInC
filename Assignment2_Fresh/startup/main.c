2fef0ad4491b4e7cf58a97863cd3f8312144e96b
VicRMIT <s3732094@student.rmit.edu.au>
2019-02-16 22:19:44 +1100

    Add fresh A2 files

---
+++ Assignment2_Fresh/startup/main.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+ * Student Name    :
+ * RMIT Student ID :
+ * COURSE CODE     :
+ *
+ * Startup code provided by Paul Miller for use in "Programming in C",
+ * study period 4, 2018.
+ *****************************************************************************/
+
+#include "main.h"
+
+int main(int argc, char* argv[]) {
+        /* the array of commands that can be used in this program */
+        struct command commands[NUM_COMMANDS];
+        /* initialise the commands */
+        /* implement behaviour based on the number of arguments passed in */
+        /* if there are invalid arguments, display appropriate
+         * error messages and exit */
+        return EXIT_FAILURE;
+}
+
+/**
+ * common function name used for explaining how to use the program
+ **/
+void print_usage(void) {
+
+}
